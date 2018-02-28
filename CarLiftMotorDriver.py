426416d087cdab51012c4417c3e3bd47329c3bd1
LonnieClifton <lclifon12@frontier.com>
2018-02-28 16:40:26 -0600

    Update to save work

--- CarLiftMotorDriver.py
+++ CarLiftMotorDriver.py
@@ -1,28 +1,30 @@
+
 #!/usr/bin/python
- 
-# Import required libraries
+
+# CarLiftMotorDriver
+# Low level driver for the lift stepper motor 
+
 import sys
 import time
 import RPi.GPIO as GPIO
  
- def driver(direction, steps, speed):
- 
- 
+def CarLiftMotorDriver(direction, RequestedSteps, waitTime):
+totalSteps = 0 
+
+# Define GPIO signals to use
+# Physical pins 11,15,16,18
+# GPIO17,GPIO22,GPIO23,GPIO24
+StepPins = [17,22,23,24]     
+
 # Use BCM GPIO references
 # instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
 
 #Define GPIO for top or bottom limit switch
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN
- 
-# Define GPIO signals to use
-# Physical pins 11,15,16,18
-# GPIO17,GPIO22,GPIO23,GPIO24
-StepPins = [17,22,23,24]
-                                   
+  
 # Set all pins as output
 for pin in StepPins:
-  # print "Setup pins"
   GPIO.setup(pin,GPIO.OUT)
   GPIO.output(pin, False)
  
@@ -41,8 +43,7 @@ StepCount = len(Seq)
 StepDir = direction    # Set to 1 or 2 for clockwise
 						# Set to -1 or -2 for anti-clockwise
 
-WaitTime = speed
-StepCounter = steps
+StepCounter = RequestedSteps
  
 # main loop
 for StepCounter in range(steps):
@@ -52,8 +53,7 @@ for StepCounter in range(steps):
       # print "  enable GPIO %i" %(xpin)
       GPIO.output(xpin, True)
     else:
-      # print " Disable GPIO %i" %(xpin)
-      GPIO.output(xpin, False)
+        GPIO.output(xpin, False)
  
   StepCounter += StepDir
   if not GPIO.input(4):
@@ -69,6 +69,6 @@ for StepCounter in range(steps):
  
   # Wait before moving on
   time.sleep(WaitTime)
+  totalSteps += 1
   
- return stepcounter
-
+ return totalSteps 
--- CarOperate.py
+++ CarOperate.py
@@ -1,18 +1,28 @@
 
 
-CarCurrentFloor = 0
-CarCurrentStatus - 'dnd'  		# floor#, moving, dnd , fault
+CarFloorsTotal = 5			# Number of floors for this elevator car 
+
+CarCurrntFloorSteps = 0			# the number of steps from the 1st floor to the current position
+CarCurrentFloor = 0			3 The current floor#
+CarCurrentStatus - 'dnd'  		# floor#, 'moving', ;dnd' , 'fault'
 CarCurrentDirection = 'stopped'  	# up, down, stopped
-CarParkFloor = 1			# usually 1st floor
+
+CarParkFloor = 1			# Floor to return when no activity, usually 1st floor
+
+# delays and timing
 CarTravelSpeed = .003 			# Car movement speed when moving up or down
 CarDoorOpenSpeed = .003			# Car door open speed
 CarDoorCloseSpeed = .003		# Car door close speed
 CarDoorCloseDelay = 10			# Car door close wait Time (seconds)
-CarTotalSteps = 0			# Car step count to move from bottom to top floor
-CarNoActivityTime = 50			# time with no car requests
-CarDispatchList = [0,0,0,0,0] 		# floor number(s) to stop.Also updated from the System manager for Hallway calls
+
+
+CarTotalStep2Top = 0			# Car step count to move from bottom to top floor
+CarNoActivityTime = 50			# time with no car requests (seconds)
+
+CarFloorStopList = [0,0,0,0,0] 		# floor number(s) to stop. Updated from the System manager for Hallway calls
 CarFloorStepList = [0,250,500,750,1000]	# the stepper motor count needed to move to a floor from the bottom floor
+					#   updated when the car is initializes for recalibrated
+
 CarFloorCount = 0			# Count of floors the car has moved
-CarFloorsTotal = 5			# Number of floors for this elevator car 
 
 import CarStartUp.py			# run the start up initialization
