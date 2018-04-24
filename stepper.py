54b2056b0ec3ee1eb84346af5df67e1c13dcb595
sdev265EV <35968375+sdev265EV@users.noreply.github.com>
2018-04-24 15:23:50 +0000

    update 20180424

--- stepper.py
+++ stepper.py
@@ -1,35 +1,33 @@
-
+#!/usr/bin/python
+ 
 # Import required libraries
 import sys
 import             time
 import RPi.GPIO as GPIO
  
-
-# Use BCM GPIO references instead of physical pin numbers
+# Use BCM GPIO references
+# instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
  
 # Define GPIO signals to use
 # Physical pins 11,15,16,18
 # GPIO17,GPIO22,GPIO23,GPIO24
-
-  GPIO.setup(17,GPIO.OUT)
-  GPIO.setup(22,GPIO.OUT)
-  GPIO.setup(23,GPIO.OUT)
-  GPIO.setup(24,GPIO.OUT)
+StepPins = [3,4,5,6]
                                    
 # Set all pins as output
-  GPIO.output(17, False)
-  GPIO.output(22, False)
-  GPIO.output(23, False)
-  GPIO.output(24, False)
+for pin in StepPins:
+  print "Setup pins"
+  GPIO.setup(pin,GPIO.OUT)
+  GPIO.output(pin, False)
  
 #set up top and bottom floor limit switches
+print ('set limit switch IO')
 GPIO.setup(7,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(8,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 totalSteps = 0
 
-# Define advanced sequence as shown in manufacturers datasheet
+# Define  sequence in manufacturers datasheet
 Seq = [[1,0,0,1],
        [1,0,0,0],
        [1,1,0,0],
@@ -39,18 +37,27 @@ Seq = [[1,0,0,1],
        [0,0,1,1],
        [0,0,0,1]]
 
+Seq3 = [[1,1,0,0],
+       [0,1,1,0],
+       [0,0,1,1],
+       [1,0,0,1]]
+
+       
 StepCount = len(Seq)
 StepDir = 1 # Set to 1 or 2 for clockwise
             # Set to -1 or -2 for anti-clockwise
 
 if not GPIO.input(7):
    print("top Limit closed")
-   StepDir = 1
+   StepDir = -1
 
 if not GPIO.input(8):
    print("bottom limit closed")
    StepDir = 1
- 
+
+#exit() 
+
+
 WaitTime = .001
  
 # Initialise variables
@@ -59,11 +66,16 @@ StepCounter = 0
 # Start main loop
 while True:
  
+  print StepCounter,
+  print Seq[StepCounter]
+ 
   for pin in range(0, 4):
     xpin = StepPins[pin]
     if Seq[StepCounter][pin]!=0:
+#      print "  enable GPIO %i" %(xpin)
       GPIO.output(xpin, True)
     else:
+#      print " Disable GPIO %i" %(xpin)
       GPIO.output(xpin, False)
  
   StepCounter += StepDir
@@ -72,6 +84,8 @@ while True:
   # start again
   if (StepCounter>=StepCount):
     StepCounter = 0
+
+  if (StepCounter<0):
     StepCounter = StepCount+StepDir
  
   # Wait before moving on
@@ -80,10 +94,10 @@ while True:
   totalSteps += 1
   print(totalSteps)
 
-  if not GPIO.input(7) and totalSteps > 100:
-    exit()
+  if not GPIO.input(7) and totalSteps > 300:
     print("bottom limit")
+    exit()
 
-  if not GPIO.input(8) and totalSteps > 100:
+  if not GPIO.input(8) and totalSteps > 300:
     print("top limit")
     exit()
