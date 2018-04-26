b61d8f17793127ab473e7f4927de35bdd3fb4bc2
lclifton@ivytech.edu <lclifton@ivytech.edu>
2018-04-26 00:00:59 +0000

    update

---
+++ config.py
@@ -0,0 +1,7 @@
+CarLampsPins=[0,19,20,21,22,23]
+FloorStopList=[0,0,0,0,0,0]
+FloorStepList=[0,0,2200,4400,5000,7000]
+CarCurrentFloor=0
+FloorCurrentStep=0
+CarCurrentDirection=0
+CarCurrentStatus='stopped'
