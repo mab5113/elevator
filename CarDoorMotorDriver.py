2a14bf5e19a3cf4aac1c104dfd3fb6b45857aa1a
LonnieClifton <lclifon12@frontier.com>
2018-04-26 12:40:39 -0500

    update

--- CarDoorMotorDriver.py
+++ CarDoorMotorDriver.py
@@ -1,26 +1,97 @@
+import time
+import RPi.GPIO as GPIO
+	
+class CarDoorDriver():
+	stepDelayTime = .004
+	currentStepPosition = 0
+	grandTotalSteps = 0
+	
+	# Define H-bridge sequence in manufacturers datasheet
+	#  Notice that from step to step, only one bit changes	
+	Seq = [[1,0,0,1], [1,0,0,0], [1,1,0,0], [0,1,0,0], [0,1,1,0], [0,0,1,0], [0,0,1,1], [0,0,0,1]]
+	SeqLength = len(Seq)
 
+	#def __init__(self): 
+	# instance variables unique to each instance
+	
+	# Use BCM GPIO references
+	# instead of physical pin numbers
+	GPIO.setmode(GPIO.BCM)
+	
+	#set up top and bottom floor limit switches
+	GPIO.setup(8,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-Def DoorOpenClose(openClose, steps, openSpeed, closeSpeed):
-If Operation = 'open':
-For i in range(0,steplimit)
-	If limitswitch closes;
-		Return ‘ok’
-##Step the motor in open direction
-# Return ‘99 door open failure’
+	# Define GPIO signals to use
+	# Physical pins 21,19,23,32
+	# GPIO9, GPIO10, GPIO11, GPIO12
+	stepPins = [9,10,11,12]
+							   
+	# Set all pins as output
+	for pin in stepPins:
+		GPIO.setup(pin,GPIO.OUT)
+		GPIO.output(pin, False)
+		
+		
+	def DoSteps(moveSteps):
+		totalSteps = 0
+		
+		# Allow for hystersis of the limit switches
+		#  It will take n steps for the limit switch to open
+		LimitSwitchHystersis = 300
 
-elIf  Operation = close:
+		if moveSteps < 0:
+			stepDir = 1
+			moveSteps = abs(moveSteps)
+			if not GPIO.input(8):
+				print("bottom limit")
+				return 0
+		else:
+			stepDir = -1
+			if not GPIO.input(13) :
+				print("top limit")
+				return 999999
+				
+		while totalSteps < moveSteps:
+			StepCounter =0
+			# This where all the action happens. There are 8 microsteps per step
+			for pin in range(0, 4):
+				xpin = StepPins[pin]
+				if Seq[StepCounter][pin]=0:
+					GPIO.output(xpin, False)
+				else:
+					GPIO.output(xpin, True)
+					
+	 
+			# Notice that stepcounter will decrease if stepDir is negative
+			StepCounter += stepDir
+			
+			# When at the end of the step sequence start again
+			if (StepCounter>=StepCount):
+				self.SeqLength = 0
+			
+			if (StepCounter<0):
+				self.SeqLength = StepCount + self.stepDir
+				
+			if self.stepDir == 1:
+				currentStepPosition -= 1
+			else:
+				currentStepPosition +=-1
+				
+			self.totalSteps += 1		# completed one motor step, count it
+			self.grandTotalSteps += 1
+				
+		
+			if not GPIO.input(7) and totalSteps > LimitSwitchHystersis:
+				print("bottom limit")
+				return 0
+			
+			if not GPIO.input(8) and totalSteps > LimitSwitchHystersis:
+				print("top limit")
+				return self.totalSteps
+				
+			# Wait before doing the next step
+			time.sleep(self.stepDelayTime)
 
-For i in range(0,steplimit)
-	If limitswitch closes;
-		Return ‘0 Door is closed’
-	If Breakbeam = true
-		Return ‘1 Break Beam Tripped’
-	If doorForceSwitch closed
-		Return ‘2 Door Blocked’
-	If cancelbuttonPressed
-			Return ‘3 Door Open Button Pressed’
+		Return self.totalSteps
 
-#Continue to Step the motor in open direction
-Return ‘99 door open failure’
-Else
-	Return ‘99 invalid door command’
--- CarDriver.py
+++ CarDriver.py
@@ -1,48 +1,28 @@
 #!/usr/bin/python
 
 import time
+import config
 import RPi.GPIO as GPIO
 from CarMotorDriver import CarMotorDriver
 
+
 def CarDriver(command,moveSteps,waitTime):
-	# Use BCM GPIO references
-	# instead of physical pin numbers
-	#GPIO.setmode(GPIO.BOARD)
+
+carDriver= CarMotorDriver()
 	
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-		
+
 	if command == 'reset':
-		rv = CarMotorDriver(-99999,waitTime)
+		rv = carDriver.DoSteps(-99999)
 		print('CarDriver CarMotorDriver: ', rv)
 		CarCurrentFloor=1
 		CarCurrentDirection=1
 		return rv
 	elif command=='move':
-		rv = CarMotorDriver(moveSteps,waitTime)
+		rv = carDriver.DoSteps(moveSteps)
 		print('CarDriver CarMotorDriver: ', rv)
 
 	else:
-	
-		print("CarDriver: ", moveSteps, waitTime)
-		stepPos
-		Direction = -1
-		#waitTime = .004
-		StepCounter = 0
-		currentStep = 0
-	
-		#set up top and bottom floor limit switches
-		GPIO.setup(7,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		GPIO.setup(8,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-		if not GPIO.input(7):
-			print("top Limit closed")
-			stepDir = -1
-		
-		elif not GPIO.input(8):
-			print("bottom limit closed")
-			stepDir = 1
-	
-		CarMotorDriver(moveSteps, waitTime)
+		CarMotorDriver(moveSteps)
 
 
 	
