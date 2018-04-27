5cae2977234709193f83320256e46c31f6d1deb9
Lonnie Clifton <lclifton@ivytech.edu>
2018-04-27 07:17:32 -0500

    20180427

--- CarDoorMotorDriver.py
+++ CarDoorMotorDriver.py
@@ -3,16 +3,21 @@ import RPi.GPIO as GPIO
 	
 class CarDoorDriver():
 	stepDelayTime = .004
-	currentStepPosition = 0
-	grandTotalSteps = 0
-	
+	currentCarPosition = 0
+	LifeTimeTotalSteps = 0
+		
+	# Allow for hystersis of the limit switches
+	#  It will take n steps for the limit switch to open
+	LimitSwitchHystersis = 300
+
 	# Define H-bridge sequence in manufacturers datasheet
 	#  Notice that from step to step, only one bit changes	
 	Seq = [[1,0,0,1], [1,0,0,0], [1,1,0,0], [0,1,0,0], [0,1,1,0], [0,0,1,0], [0,0,1,1], [0,0,0,1]]
 	SeqLength = len(Seq)
 
 	#def __init__(self): 
-	# instance variables unique to each instance
+		# instance variables unique to each instance
+		pass
 	
 	# Use BCM GPIO references
 	# instead of physical pin numbers
@@ -26,20 +31,29 @@ class CarDoorDriver():
 	# Physical pins 21,19,23,32
 	# GPIO9, GPIO10, GPIO11, GPIO12
 	stepPins = [9,10,11,12]
-							   
-	# Set all pins as output
+
+	# Set all H-Bridge Raspberry pins as output
 	for pin in stepPins:
 		GPIO.setup(pin,GPIO.OUT)
 		GPIO.output(pin, False)
-		
-		
-	def DoSteps(moveSteps):
-		totalSteps = 0
-		
-		# Allow for hystersis of the limit switches
-		#  It will take n steps for the limit switch to open
-		LimitSwitchHystersis = 300
 
+	def SetstepDelayTime(stepDelay):
+		self.stepDelayTime = self.stepDelay
+	
+	def GetcurrentCarPosition():
+		return self.currentCarPosition 
+		
+	def IsAtBottom:
+		if not GPIO.input(7):
+		return True
+
+	def IsAtTop:
+		if not GPIO.input(8):
+		return True
+
+	def DoSteps(moveSteps):
+		self.totalSteps = 0
+		
 		if moveSteps < 0:
 			stepDir = 1
 			moveSteps = abs(moveSteps)
@@ -50,21 +64,22 @@ class CarDoorDriver():
 			stepDir = -1
 			if not GPIO.input(13) :
 				print("top limit")
-				return 999999
+				return self.currentCarPosition
 				
 		while totalSteps < moveSteps:
 			StepCounter =0
-			# This where all the action happens. There are 8 microsteps per step
+
+			# This where all the action happens. 
+			# There are 4 pins, 8 microsteps per step
 			for pin in range(0, 4):
 				xpin = StepPins[pin]
 				if Seq[StepCounter][pin]=0:
 					GPIO.output(xpin, False)
 				else:
 					GPIO.output(xpin, True)
-					
-	 
+ 
 			# Notice that stepcounter will decrease if stepDir is negative
-			StepCounter += stepDir
+			self.StepCounter += self.stepDir
 			
 			# When at the end of the step sequence start again
 			if (StepCounter>=StepCount):
@@ -73,21 +88,18 @@ class CarDoorDriver():
 			if (StepCounter<0):
 				self.SeqLength = StepCount + self.stepDir
 				
-			if self.stepDir == 1:
-				currentStepPosition -= 1
-			else:
-				currentStepPosition +=-1
-				
+			self.currentCarPosition += stepDir
+						
 			self.totalSteps += 1		# completed one motor step, count it
-			self.grandTotalSteps += 1
-				
+			self.LifeTimeTotalSteps += 1	
 		
 			if not GPIO.input(7) and totalSteps > LimitSwitchHystersis:
-				print("bottom limit")
+				self.currentCarPosition = 0
+				print("At bottom limit")
 				return 0
 			
 			if not GPIO.input(8) and totalSteps > LimitSwitchHystersis:
-				print("top limit")
+				print("At top limit")
 				return self.totalSteps
 				
 			# Wait before doing the next step
