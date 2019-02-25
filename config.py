95c87338c4cb8adeff1e432ed9e9cc2bd128f671
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-02-25 09:07:00 -0600

    Updated

--- config.py
+++ config.py
@@ -1,21 +1,28 @@
 test=''
-CarLampsPins=[0,19,20,21,22,23]
 
-FloorStopList=[0,0,1,0,0,0]
-FloorStopListUP=[0,0,0,0,0,0]
-FloorStopListDOWN=[0,0,0,0,0,0]
+FloorStopList = [0,0,1,0,0,0]
+FloorStopListUP = [0,0,0,0,0,0]
+FloorStopListDOWN = [0,0,0,0,0,0]
 
-FloorStepList=[0,0,2200,4400,5000,7000]
+car1CurrentFloor =  [0,0,0,0,0,0]
 
-CarCurrentFloor=0
-FloorCurrentStep=0
-CarCurrentDirection=0
-CarCurrentStatus='stopped'
+FloorStepList=[0,-10000,2200,4400,5000,10000]
+
+CarCurrentFloor = 0
+FloorCurrentStep = 0
+CarCurrentDirection = 0
+CarCurrentStatus = 'stopped'
 CarCurrentStepPosition = 0
 CarStepWaitTime = .002
 CarTopPosition = 7400
 
-HallipAddress='10.81.104.120'
-CaripAddress1='10.81.104.121'
-CaripAddress2='10.81.104.122'
+HallipAddress = '10.81.104.120'
+CaripAddress1 = '10.81.104.121'
+CaripAddress2 = '10.81.104.122'
 
+CarFloorLampsPins=[0,19,20,21,22,23]
+carFloorLamp1= 19
+carFloorLamp2 = 20
+carFloorLamp3 = 21
+carFloorLamp4 = 22
+carFloorLamp5 = 23
