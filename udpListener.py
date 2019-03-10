054cbbb29879d0dd74a27e930e654d89f076d881
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-10 18:25:31 -0500

    Update udpListener.py

--- udpListener.py
+++ udpListener.py
@@ -21,9 +21,9 @@ class myThread (threading.Thread):
 
 
 def myListener():
-	UDP_IP = "192.168.254.69"
-#	UDP_IP = "0.0.0.0"
-	UDP_PORT = 5006
+#	UDP_IP = "192.168.254.69"
+	UDP_IP = "0.0.0.0"
+	UDP_PORT = 5005
  
 	sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP Datagram
@@ -49,6 +49,8 @@ def myListener():
 threadlock = threading.Lock()
 thread1= myThread(1,"Thread1", 1)
 thread1.start()
+print ("def myListener: udp listener thread started")
+
 
 #text = "start"
 #while text <> "end":
