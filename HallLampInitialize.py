57b6abaa7aedf9a713216792e5638e0b1c4e3163
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-10 15:04:35 -0500

    Update and rename HallLampInitializexx.py to HallLampInitialize.py

--- HallLampInitialize.py
+++ HallLampInitialize.py
@@ -2,9 +2,7 @@
 #   Describe this Module
 #   Use pin names in config - no magic pin numbers
 
-import time
 import RPi.GPIO as GPIO
-from HallLampCallBack import HallLampCallBack
 
 def HallLampInitialize():
 
@@ -14,29 +12,43 @@ def HallLampInitialize():
 	
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
-		
+
+	HallDownLampPinID = [11,12,13,14] #No floor 1 Down
+	HallUpLampPinID   = [15,16,17,18] #No floor 5 Up
+	
+	#Down Lamps setup
 	GPIO.setup(11,GPIO.OUT)
 	GPIO.setup(12,GPIO.OUT)
 	GPIO.setup(13,GPIO.OUT)
 	GPIO.setup(14,GPIO.OUT)
+	
+	#Up Lamps setup
 	GPIO.setup(15,GPIO.OUT)
 	GPIO.setup(16,GPIO.OUT)
 	GPIO.setup(17,GPIO.OUT)
 	GPIO.setup(18,GPIO.OUT)
 
+	# Turn on the Down Lamps by pulling the output to ground
 	GPIO.output(11,False)
 	GPIO.output(12,False)
 	GPIO.output(13,False)
 	GPIO.output(14,False)
+	
+	# Turn on the Up Lamps
 	GPIO.output(15,False)
 	GPIO.output(16,False)
 	GPIO.output(17,False)
 	GPIO.output(18,False)
+	
 	time.sleep(2)
+	
+	# Turn off the Down Lamps
 	GPIO.output(11,True)
 	GPIO.output(12,True)
 	GPIO.output(13,True)
 	GPIO.output(14,True)
+	
+	# Turn off the Down Lamps
 	GPIO.output(15,True)
 	GPIO.output(16,True)
 	GPIO.output(17,True)
