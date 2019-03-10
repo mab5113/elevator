f6b02ff5403aac6dda351cc3e467fc2e8e894c7c
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-10 18:17:36 -0500

    Update udpListener.py

--- udpListener.py
+++ udpListener.py
@@ -21,8 +21,8 @@ class myThread (threading.Thread):
 
 
 def myListener():
+	UDP_IP = "192.168.254.69"
 #	UDP_IP = "0.0.0.0"
-	UDP_IP = "0.0.0.0"
 	UDP_PORT = 5006
  
 	sock = socket.socket(socket.AF_INET, # Internet
