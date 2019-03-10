4447f619ab41ebe6d5bdb29a11dde06af79912e7
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-10 15:31:07 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,24 +1,23 @@
 # Global identifiers to be imported int module
 
-FloorStopList = [0,0,0,0,0,0]
-HallStopList  = [0,0,0,0,0,0]
+FloorStopList   = [0,0,0,0,0,0]
+HallStopList    = [0,0,0,0,0,0]
+carCurrentFloor =  [0,0,0,0,0,0]
 
-# Car1 is position 1 in the list
-# Car 2 is postion 2 in the list
-#  etc.
-# Might encode status also
-#   1= going up
-#  -1 = going down
-#   0 = no current calls
-# Can handle as many cars approprate
-#   each car has an entry
-#   Example: [0,0,-1,0,0,0]
-#     Car1 has no calls
-#     Car2 is on floor 2 and is going down
 
-car1CurrentFloor =  [0,0,0,0,0,0]
+# floor positions for the stepper motor
+#  convet to lists (of lists) so each car can have its own list
+FloorStepList=[0,-10000,2200,4400,5000,10000]
 
-IpHall='10.81.104.31/24'
+CarCurrentStatus = 'stopped'
+CarCurrentStepPosition = 0
+CarStepWaitTime = .0015
+CarTopPosition = 7400
+
+CarFloorLampsPins=[0,19,20,21,22,23]
+IpHall='192.168.254.69/24'
+
+#IpHall='10.81.104.31/24'
 IpCar1='10.81.104.32/24'
 IpCar2='10.81.104.33/24'
 IpCar3='10.81.104.34/24'
@@ -29,21 +28,3 @@ IpSean='10.81.104.28/24'
 IpRyan='10.81.104.29/24'
 IpChristian='10.81.104.30/24'
 
-# floor positions for the stepper motor
-#  convet to lists (of lists) so each car can have its own list
-FloorStepList=[0,-10000,2200,4400,5000,10000]
-
-CarCurrentFloor = 0
-FloorCurrentStep = 0
-CarCurrentDirection = 0
-CarCurrentStatus = 'stopped'
-CarCurrentStepPosition = 0
-CarStepWaitTime = .0015
-CarTopPosition = 7400
-
-CarFloorLampsPins=[0,19,20,21,22,23]
-carFloorLamp1= 19
-carFloorLamp2 = 20
-carFloorLamp3 = 21
-carFloorLamp4 = 22
-carFloorLamp5 = 23
