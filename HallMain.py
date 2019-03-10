1f9b04e69b1b0131e07561905ce7892a8b12dc1d
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-10 14:47:03 -0500

    Update HallMain.py

--- HallMain.py
+++ HallMain.py
@@ -1,11 +1,14 @@
-	try:   	
-		while True:
-			time.sleep(.5)
-			#print ('.')
-			pass # do the loop here
-	except KeyboardInterrupt:
-		# This will catch the ctrl-c to allow clean-up
-		print ('Done')
-		# do cleanup here
+
+
+HallLampInitialize()
+
+try:   	
+	while True:
+		time.sleep(.5)
+		#print ('.')
 		pass 
-		
+except KeyboardInterrupt:
+	# This will catch the ctrl-c to allow clean-up
+	print ('Done')
+	# do cleanup here
+	pass 
