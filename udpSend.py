eca28ba865729035cb7172e086e3651be25f33e6
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-10 19:39:23 -0500

    Update udpSend.py

--- udpSend.py
+++ udpSend.py
@@ -4,7 +4,7 @@ import time
 def send(message):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.sendto(message.encode(), ("127.0.0.1", 5005))
+	sock.sendto(message.encode(), ("192.168.254.69", 5005))
 
 send('6')
 time.sleep(.3)
