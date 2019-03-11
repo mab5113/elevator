171f1274d315d15d641809493b9e6f07573f7a60
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-10 21:01:06 -0500

    Update udpListener.py

--- udpListener.py
+++ udpListener.py
@@ -40,13 +40,24 @@ def myListener():
 			print ("Received quit message, exiting udp Listener")
 			thread1.stop()
 			pass
-		
-		#print (addr)
-		#if msg.startswith('hbp'):
 		s = msg.split(',', 1)
-		print (len(s))
-		print (s)
-		#HallButtonCallBack(int(msg))
+		#print (len(s))
+		#print (s)
+		#print (addr)
+		if msg.startswith('hbp'):
+			floor = s[1]
+			if   floor == '-2': pin = 3
+			elif floor == '-3': pin= 4
+			elif floor == '-4': pin= 5
+			elif floor == '-5': pin= 6
+
+			#UP BUTTONS (there is no floor 5 up button)
+			elif floor ==  '1': pin=7
+			elif floor ==  '2': pin=8
+			elif floor ==  '3': pin=9
+			elif floor ==  '4': pin=10
+			HallButtonCallBack(pin)
+			
 		#config.test=msg
 		#print (config.test)
 
