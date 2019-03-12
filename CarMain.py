d757a705fabd846750262644fa8b517ac2594908
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-12 12:58:37 -0500

    Update CarMain.py

--- CarMain.py
+++ CarMain.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
-# imports and runs the different classes used to move the car and light up the lamps
-# imports the CarLampMonitor Class from CarLampMonitor.py
-# imports the CarManager class from ClassManager.py
+#runs setup programs
  
 # Import required libraries
-# import config
-# import time
-
 from CarManager import CarManager
 from CarLampMonitor import CarLampMonitor
 from udpListener import udpListener
