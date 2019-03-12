416dc071896bee40d65831ce4130785f667f32de
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-12 16:06:57 -0500

    Create CarLampInitialize.py

---
+++ CarLampInitialize.py
@@ -0,0 +1,30 @@
+
+def CarLampInitialize():
+
+	# Use BCM GPIO references
+	# instead of physical pin numbers
+	#GPIO.setmode(GPIO.BOARD)
+	
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+		
+	GPIO.setup(19,GPIO.OUT)
+	GPIO.setup(20,GPIO.OUT)
+	GPIO.setup(21,GPIO.OUT)
+	GPIO.setup(22,GPIO.OUT)
+	GPIO.setup(23,GPIO.OUT)
+	
+	GPIO.output(19,False)
+	GPIO.output(20,False)
+	GPIO.output(21,False)
+	GPIO.output(22,False)
+	GPIO.output(23,False)
+
+	time.sleep(2)
+
+	GPIO.output(19,True)
+	GPIO.output(20,True)
+	GPIO.output(21,True)
+	GPIO.output(22,True)
+	GPIO.output(23,True)
+	
