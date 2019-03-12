46e33e0e8e9d1fc74a8f61b4601161da2cd37204
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-12 16:11:48 -0500

    Update and rename CarLampMonitor.py to CarButtonInitialize.py

--- CarButtonInitialize.py
+++ CarButtonInitialize.py
@@ -14,26 +14,6 @@ def CarLampMonitor():
 	GPIO.setmode(GPIO.BCM)
 	GPIO.setwarnings(False)
 		
-	GPIO.setup(19,GPIO.OUT)
-	GPIO.setup(20,GPIO.OUT)
-	GPIO.setup(21,GPIO.OUT)
-	GPIO.setup(22,GPIO.OUT)
-	GPIO.setup(23,GPIO.OUT)
-	
-	GPIO.output(19,False)
-	GPIO.output(20,False)
-	GPIO.output(21,False)
-	GPIO.output(22,False)
-	GPIO.output(23,False)
-
-	time.sleep(3)
-
-	GPIO.output(19,True)
-	GPIO.output(20,True)
-	GPIO.output(21,True)
-	GPIO.output(22,True)
-	GPIO.output(23,True)
-	
 	GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 	GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 	GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
