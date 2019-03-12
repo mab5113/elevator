9f0a52ae49a30a3492dc31549a085ab3e6c33ef1
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-12 13:09:59 -0500

    Update StartUp.py

--- StartUp.py
+++ StartUp.py
@@ -1,47 +1,46 @@
+!/usr/bin/python3
 #StartUp.py 
 # Determines the role of this RPi by reading jumpers on two inputs
 
 import config
 import os
 import RPi.GPIO as GPIO
-#from HallMain import HallMain
-#from CarMain import CarMain
+from CarMain import CarMain
 
 IDPin1=24
 IDPin2=25
-
 
 # Use BCM GPIO references instead of physical pin numbers
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-#Check device ID
+#Setup RPi device I/O
 GPIO.setup(24,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(25,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
-# Read IO port if find ID
+# Read IO ports if find ID
 id1in = GPIO.input(24)
 id2in = GPIO.input(25)
+
+# Switch closure will pull the port to ground (0 Volts)
+#  This same as logic false
+
 print ('StartUp: id1in: ', id1in)
 print ('StartUp: id1in: ', id2in)
 
-if not GPIO.input(24):
-	id1=1
-else:
-	id1=0
+# Need to change switch closure to True using not
+if not GPIO.input(24): id1=1
+else: 	id1=0
 
-if not GPIO.input(25):
-	id2=1
-else:
-	id2=0
+if not GPIO.input(25): 	id2=1
+else: 			id2=0
 
 #Convert inputs  to a binary number
 id= id1 + id2 * 2
 
 print(' ID1:', id1, '  ID2:', id2, '   ID:', id)
 
-print (config.FloorStopList)
+print ('StartUp: floorStopList; ', config.FloorStopList)
 
 if id == 0:
 	ipAddress=config.IpHall
@@ -59,7 +58,6 @@ elif id == 1:
 	os.system('sudo ifconfig eth0 up')
 	print ('')
 	print('StartUp: Car Main 1 ID found --> Starting CarMain()')
-	from CarMain import CarMain
 	CarMain(id,ipAddress)
 
 	#Car ID and ip address are passed as arguments to car main function
@@ -76,7 +74,6 @@ elif id == 2:
 elif id == 3:
 	ipAddress=IpCar3
 	os.system('sudo ifconfig eth0 down')
-
 	os.system('sudo ifconfig eth0 ' + ipAddress)
 	os.system('sudo ifconfig eth0 up')
 	print ('')
