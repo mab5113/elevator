ba27f67b0f4529d27b432fe550a364f5644c60e2
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-15 20:29:19 -0500

    Update HallMain.py

--- HallMain.py
+++ HallMain.py
@@ -1,20 +1,22 @@
+#!/usr/bin/python3
 
 from HallButtonInitialize import HallButtonInitialize
 from HallLampInitialize import HallLampInitialize
 from udpListener import myListener
 
-HallLampInitialize()
-HallButtonInitialize()
-myListener()
 
-try:   	
-	while True:
-		time.sleep(.5)
-		#print ('.')
+def HallMain(id):
+	HallLampInitialize(id)
+	HallButtonInitialize(id)
+	myListener(id)
+
+	try:   	
+		while True:
+			time.sleep(.5)
+			#print ('.')
+			pass 
+	except KeyboardInterrupt:
+		# This will catch the ctrl-c to allow clean-up
+		print ('Done')
+		# do cleanup here
 		pass 
-except KeyboardInterrupt:
-	# This will catch the ctrl-c to allow clean-up
-	
-	print ('Done')
-	# do cleanup here
-	pass 
