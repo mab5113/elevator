77e3adb68e5bff7d348836436a312bd597ae84f6
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-16 08:14:15 -0500

    Update CarInitialize.py

--- CarInitialize.py
+++ CarInitialize.py
@@ -9,8 +9,19 @@ from CarButtonInitialize import CarButtonInitialize
 from udpListener import *
 	
 def CarInitialize(id):
-	print ('Starting CarMain')
+	print ('Starting CarInitialize for car #', id)
+	
+	#TODO: change to simple loop (for any list length)
+	config.CarFloorStopList[0] = 0
+	config.CarFloorStopList[1] = 0
+	config.CarFloorStopList[2] = 0
+	config.CarFloorStopList[3] = 0
+	config.CarFloorStopList[4] = 0
+	config.CarFloorStopList[5] = 0
+	
 	CarButtonInitialize(id)
 	CarLampInitialize(id)
 	udpListenerMain(id)
 	CarManager(id)
+
+	print ('Starting CarInitialize for car #', id)
