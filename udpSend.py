64859111ff32e27f8aa638ed6fba911c5153a472
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-18 11:43:13 -0700

    Update udpSend.py

--- udpSend.py
+++ udpSend.py
@@ -1,16 +1,16 @@
 import socket
 # import time
 
-def send(message, ip = "192.168.254.69"):
+def send(message, ip = "127.0.0.1"):
 	port = 5005
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	
 	sock.sendto(message.encode(), (ip, port))
 
-#floor = ""
-#while floor != "-99":
-#	floor = str(input("Floor Number: "))
-#	send(floor)
-# time.sleep(.3)
+c =1
+while msg != "":
+	send(c)
+	c += 1
+	time.sleep(1)
 #send("quit")
