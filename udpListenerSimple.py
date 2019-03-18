698da1cd2b8fa2df0b1d419cd4658feb4b7d04c8
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-18 12:21:52 -0700

    Update udpListenerSimple.py

--- udpListenerSimple.py
+++ udpListenerSimple.py
@@ -34,6 +34,10 @@ def myListener():
 		msg, addr = sock.recvfrom(1024)	# buffer size is 1024 bytes
 		msg = msg.decode('utf-8')
 		print (msg)
+		a,b,c = msg.split(',')
+		print (a)
+		print (b)
+		print (c)
 		if msg == 'quit':
 			print ("Received quit message, exiting udp Listener")
 			thread1.stop()
