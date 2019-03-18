3c1d14f3edb1c6959f6558858c2e0e44a73901d0
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-17 22:31:39 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,18 +7,14 @@ RoleIdPin2 = 0
 FloorStopList    = [0,0,0,0,0,0]
 CarFloorStopList = [0,0,0,0,0,0]
 
-CarCurrentFloor  = [0,0,0,0,0,0]
+CarCurrentFloor  = 0
+CarCurrentDirection = 0           # 0=stopped, 1=up, -1=down
 
 CarLampsPins  = [0,19,20,21,22,23]
 CarButtonPins = [0,14,15,16,17,18]
 
-CarLiftLimitSwitchBottom = 0
-CarLiftLimitSwitchTop = 0
-CarDoorLimitSwitchClosed = 0
-CarDoorLimitSwitchOpened = 0
-
-CarCurrentStatus = [0,0,0,0,0,0,0,0]
-CarCurrentStepPosition = [0,0,0,0,0,0,0,0]
+CarCurrentStatus = 0
+CarCurrentStepPosition = 0
 CarStepWaitTime = [0, .0015, .0015, .0015, .0015, .0015, .0015, .0015, .0015]
 CarTopPosition = [0,7400,7400,7400,7400,7400,7400,7400,7400,7400,7400]
 
@@ -29,12 +25,6 @@ FloorStepList = [0,-10000,2200,4400,5000,10000]
 # Hall Values
 HallStopListUp   = [0,0,0,0,0,0]
 HallStopListDown = [0,0,0,0,0,0]
-
-HallLampPinsDown = []
-HallLampPinsUp   = []
-
-HallButtonPinsDown = []
-HallButtonPinsUp  =[]
 
 IpHall='192.168.254.69/24'
 
