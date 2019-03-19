7b1ba171898a5ca462c34c5da2f4684a92b99d45
sdev265ev <35968375+sdev265ev@users.noreply.github.com>
2019-03-19 15:15:43 -0700

    Create CarDoorManager.py

---
+++ CarDoorManager.py
@@ -0,0 +1,9 @@
+'''Door Manager Functions
+   Open door until open switch closes
+    Wait time period
+    start closing door
+    while door close switch is open
+        if door is blocked
+            Open door until open switch closes
+            wait time period
+           
